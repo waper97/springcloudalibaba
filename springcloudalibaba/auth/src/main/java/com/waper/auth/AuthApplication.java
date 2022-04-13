b49ab8a35950c0c5b11a9ff9fe1afd98152cca1a
waper97 <563667426@qq.com>
2022-04-13 22:36:52 +0800

    添加认证模块

--- springcloudalibaba/auth/src/main/java/com/waper/auth/AuthApplication.java
+++ springcloudalibaba/auth/src/main/java/com/waper/auth/AuthApplication.java
@@ -1,9 +1,11 @@
 package com.waper.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.waper.auth"})
 public class AuthApplication {
 
     public static void main(String[] args) {
